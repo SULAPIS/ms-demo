8fb51aee30f71123d210d0687a2ef3ccdb635d2e
da pa <sulapis@outlook.com>
2023-02-17 04:08:59 +0800

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -4,17 +4,3 @@ APP_NAME="rust-server"
 
 echo "Stopping ${APP_NAME} server"
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# pm2 describe ${APP_NAME} > /dev/null
-# RUNNING=$?
-
-# if [ "${RUNNING}" -ne 0 ]; then
-#   echo "Skip. Server ${APP_NAME} is not running"
-# else
-#   pm2 stop ${APP_NAME}
-# fi;
-
-# kill -9 $(lsof -i:3030 -t) 2> /dev/null
